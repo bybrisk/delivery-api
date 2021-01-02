be526d5f11d60bd7c80623cc12ebb6ff1a94a349
bybrisk <imctobybrisk@gmail.com>
2021-01-02 13:01:38 +0530

    handler modified

--- handlers/docs.go
+++ handlers/docs.go
@@ -42,14 +42,6 @@ type errorValidationWrapper struct {
 type noContentResponseWrapper struct {
 }
 
-// Details of a single delivery
-// swagger:response deliveryGetResponse
-type accountGetResponseWrapper struct {
-	// Details of a existing Business Account
-	// in: body
-	Body data.BusinessAccountResponse
-}
-
 // Success message on a single Delivery addition
 // swagger:response deliveryPostResponse
 type accountPostResponseWrapper struct {
@@ -64,27 +56,5 @@ type createAccountParamsWrapper struct {
 	// Note: the id field is ignored by create operations
 	// in: body
 	// required: true
-	Body data.AddDeliveryRequest
-}
-
-// swagger:parameters addDeliveryWithoutGeocode
-type createAccountParamsWrapper struct {
-	// Delivery data structure to add delivery without Geocode.
-	// Note: the id field is ignored by create operations
-	// in: body
-	// required: true
-	Body data.AddDeliveryWithoutGeocodeRequest
-}
-
-// swagger:parameters getDelivery
-type deliveryIDParamsWrapper struct {
-	// The deliveryID of the Business Account 
-	// in: path
-	// required: true
-	ID string `json:"id"`
-
-	// The date of delivery placed
-	// in: path
-	// required: true
-	DateOfDelivery string `json:"dateOfDelivery"`
+	Body data.AddDeliveryRequestWithGeoCode
 }
--- handlers/go.mod
+++ handlers/go.mod
@@ -1,3 +1,5 @@
 module github.com/bybrisk/delivery-api/handlers
 
 go 1.12
+
+require github.com/bybrisk/delivery-api/data v0.0.0-20210102072603-ddda7af5e1f1
